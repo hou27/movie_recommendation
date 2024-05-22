8a7d7059d6f88954de08a002a019d4ca564a53d0
hou27 <ataj125@gmail.com>
2024-05-22 13:08:40 +0900

    Control duplicate interaction data

--- server/service/interaction_service.py
+++ server/service/interaction_service.py
@@ -8,14 +8,14 @@ class InteractionService:
 
     async def survey_result(self, user_id: int, movie_id_list: list) -> ResponseDto:
         for movie_id in movie_id_list:
-            self.interactions = pd.concat([self.interactions, pd.DataFrame([[user_id, movie_id]], columns=["user_id", "movie_id"])])
+            if self.interactions.loc[(self.interactions['user_id'] == user_id) & (self.interactions['movie_id'] == movie_id)].empty:
+                self.interactions = pd.concat([self.interactions, pd.DataFrame([[user_id, movie_id]], columns=["user_id", "movie_id"])])
 
         self.interactions.to_csv("./data/interactions.csv", index=False)
 
         return ResponseDto(
                 status=201,
-                message="Add Survey Result Successfully",
-                data=None
+                message="Add Survey Result Successfully"
             )
 
     async def control_like(self, user_id: int, movie_id: int) -> ResponseDto:
@@ -28,6 +28,5 @@ class InteractionService:
 
         return ResponseDto(
                 status=201,
-                message="Control Like Successfully",
-                data=None
+                message="Control Like Successfully"
            )
