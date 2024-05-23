3f8153c966c9fdc8d9ef0a4bf9747660e6b78008
hou27 <ataj125@gmail.com>
2024-05-23 21:23:05 +0900

    Update linkpredictor structure

--- server/model/models.py
+++ server/model/models.py
@@ -27,26 +27,30 @@ class GCNLinkPredictor(nn.Module):
 
 # 유저 - 영화 간 edge를 입력받아 유저-영화 간 관계 예측
 class LinkPredictor(nn.Module):
-    # 비선형으로 개선
     def __init__(self, input_dim):
         super().__init__()
-        self.fc1 = nn.Linear(input_dim * 2, input_dim)
-        self.fc2 = nn.Linear(input_dim, 1)
+        self.fc1 = nn.Linear(input_dim * 2, input_dim * 2)
+        self.fc2 = nn.Linear(input_dim * 2, input_dim)
+        self.fc3 = nn.Linear(input_dim, input_dim)
+        self.fc4 = nn.Linear(input_dim, 1)
         self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, z, edge_index):
         row, col = edge_index
         z_row = z[row]
         z_col = z[col]
-        
         # 임베딩 연결
         z_concat = torch.cat([z_row, z_col], dim=1)
         
         # 비선형 활성화 함수
         x = F.relu(self.fc1(z_concat))
-        
         # 정규화를 위한 드롭아웃
         x = self.dropout(x)
+
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)
         
-        # 두 번째 선형 계층 및 시그모이드 활성화 적용
-        return torch.sigmoid(self.fc2(x))
+        return torch.sigmoid(self.fc4(x))
