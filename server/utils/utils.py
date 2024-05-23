8eff9c5e9c40463f0ffa9e271db032e772769332
hou27 <ataj125@gmail.com>
2024-05-23 23:05:18 +0900

    Fix index error occurring during genre-based recommendations
    The problem occurred because I tried to access the original index after retaining only the movie features corresponding to the genre.

--- server/utils/utils.py
+++ server/utils/utils.py
@@ -7,7 +7,7 @@ def create_new_user_embedding(movie_features, interacted_movie_indices):
     return new_user_embedding
 
 # 새로운 유저에 대한 영화 추천
-def recommend_movies_for_new_user(link_predictor, node_embeddings, edge_index, num_users = 1, num_movies = 9525, num_recommendations=5):
+def recommend_movies_for_new_user(link_predictor, node_embeddings, edge_index, num_users = 1, num_movies = 9525, num_recommendations=5, genre_indexs=None):
     movie_indices = torch.arange(num_users, num_users + num_movies) # 영화 인덱스 생성 (유저 수만큼 offset)
     
     # user-movie pairs 생성
@@ -16,6 +16,9 @@ def recommend_movies_for_new_user(link_predictor, node_embeddings, edge_index, n
     # 기존 유저가 본 영화 제외
     already_seen_movies = edge_index[1] - num_users
     mask = torch.ones(num_movies, dtype=torch.bool)
+    if genre_indexs is not None:
+        mask = torch.zeros(num_movies, dtype=torch.bool) # 모든 영화에 대해 False로 초기화
+        mask[genre_indexs] = True # 해당 장르의 영화만 True로 변경    
     mask[already_seen_movies] = False
     user_movie_pairs = user_movie_pairs[:, mask]
     
